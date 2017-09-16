18503a0947384d8906bdc887e2054ff772714130
Itzik ben hutta <itzikbenhutta@isaacben.fios-router.home>
2017-09-16 15:37:50 -0400

    minor change

--- functions.php
+++ functions.php
@@ -1,13 +1,15 @@
 <?php 
 
 function theme_styles() {
-    wp_enqueue_style( 'vendor_css', get_template_directory_uri() . '/public/css/vendor.css' );    
+    wp_enqueue_style( 'vendor_css', get_template_directory_uri() . '/public/css/vendor.css' );
+    //To avoid caching I use time, remove it in production.   
     wp_enqueue_style( 'theme_css', get_template_directory_uri() . '/public/css/app.css?='.time() );
 }
 add_action( 'wp_enqueue_scripts', 'theme_styles' );
 
 function theme_js() {
     wp_enqueue_script( 'vendor_js', get_template_directory_uri() . '/public/js/vendor.js', '', '', true );    
+    //To avoid caching I use time, remove it in production.    
     wp_enqueue_script( 'theme_js', get_template_directory_uri() . '/public/js/app.js?='.time(), array('vendor_js'), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'theme_js' );
